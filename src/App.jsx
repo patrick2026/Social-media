203df22bead59c557a8293bc435f5843244013cf
patrick2026 <patrickventura31@gmail.com>
2025-03-04 12:05:21 -0300

    Finalizando a Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import { Header } from "./components/header.jsx";
 import { Post } from "./Post.jsx";
+import { Sidebar } from "./components/Sidebar.jsx";
+
+import styles from "./components/App.module.css";
+
+import "./global.css";
+
 export function App() {
   return (
     <div>
       <Header />
-      <Post author="Patrick" content="Hello world" />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post author="Patrick Ventura" content="Fala galeraa 👋" />
+          <Post author="Patrick Ventura" content="Fala galeraa 👋" />
+        </main>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,33 @@
+import { PencilLine } from "phosphor-react"; // phosphor-react
+
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://plus.unsplash.com/premium_photo-1682145730713-34bba6d3d14a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt=""
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/patrick2026.png"
+          alt=""
+        />
+
+        <strong>Patrick Ventura</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
