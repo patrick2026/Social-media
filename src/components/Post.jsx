fa7c9b8d2251698043ef1f1672d0ac87045442ae
patrick2026 <patrickventura31@gmail.com>
2025-03-06 14:40:13 -0300

    Validando formulário

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -35,7 +35,12 @@ export function Post({ author, publishedAt, content }) {
   }
 
   function handleCreateNewCommentChange() {
+    event.target.setCustomValidity("");
     setNewCommentText(event.target.value);
+  }
+
+  function handleNewCommentInvalid(event) {
+    event.target.setCustomValidity("Esse campo é obrigatório");
   }
 
   function deleteComment(commentToDelete) {
@@ -44,6 +49,8 @@ export function Post({ author, publishedAt, content }) {
     });
     setComments(commentsWithoutDeletedOne);
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
 
   return (
     <article className={styles.post}>
@@ -86,10 +93,14 @@ export function Post({ author, publishedAt, content }) {
           placeholder="Deixe um comentário"
           value={newCommentText}
           onChange={handleCreateNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type="submit">Publicar</button>
+          <button type="submit" disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
         </footer>
       </form>
 
